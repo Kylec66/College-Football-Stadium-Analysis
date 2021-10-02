1de2edd246e30913aabf0632d5cbf03354a55ed8
Kyle Carlson <83102597+Kylec66@users.noreply.github.com>
2021-10-02 10:54:52 -0700

    Add files via upload

---
+++ 1-Base-Logic.js
@@ -0,0 +1,26 @@
+// Creating the map object
+var myMap = L.map("map", {
+  center: [38.8298, -96.0323],
+  zoom: 5
+});
+// Adding the tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
+
+var data =""
+
+d3.json(link).then(function(data) {
+  // Creating a GeoJSON layer with the retrieved data
+  L.geoJson(data, {
+    // Styling each feature (in this case, a neighborhood)
+    style: function(feature) {
+      return {
+        color: "white",
+        // Call the chooseColor() function to decide which color to color our neighborhood. (The color is based on the borough.)
+        fillColor: chooseColor(feature.properties.borough),
+        fillOpacity: 0.5,
+        weight: 1.5}
+      }
+  })
+})
