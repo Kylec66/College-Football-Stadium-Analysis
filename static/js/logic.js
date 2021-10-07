55166bb265bfc4cf272e715f1fa602d8f4eab6db
Kyle Carlson <83102597+Kylec66@users.noreply.github.com>
2021-10-07 13:32:40 -0700

    Add files via upload

--- static/js/logic.js
+++ static/js/logic.js
@@ -3,21 +3,40 @@ d3.json("/api/stadiums").then((data) => {
     console.log(data)
   
     // $(document).ready(function() {
-    $('#example').DataTable( {
-        data: data['table'],
+    $('#example').DataTable({
+        data: data,
         columns: [
             { title: "Stadium" },
+            { title: "Latitude" },
+            { title: "Longitude" },
             { title: "City" },
             { title: "State" },
             { title: "Team" },
             { title: "Conference" },
             { title: "Capacity" },
-            { title: "Latitude" },
-            { title: "Longitude" }
         ]
-    } );
+    });
   })
   
+d3.json("/api/record").then((data) => {
+
+    console.log(data)
+  
+    // $(document).ready(function() {
+    $('#example').DataTable({
+        data: data,
+        columns: [
+            { title: "Team" },
+            { title: "Won" },
+            { title: "Lost" },
+            { title: "Tied" },
+            { title: "Percentage" },
+            { title: "Years" },
+            { title: "Total_Games" },
+            { title: "Conference" },
+        ]
+    });
+  })
   function buildStadiumChart(stadium) {
   
     console.log(stadium);
@@ -26,7 +45,7 @@ d3.json("/api/stadiums").then((data) => {
   
       var trace1 = {
         type: 'bar',
-        x: data['Team'],
+        x: data['Stadium'],
         y: data['Capacity'],
         marker: {
             color: '#C8A2C8',
@@ -36,7 +55,7 @@ d3.json("/api/stadiums").then((data) => {
       var data = [ trace1 ];
       
       var layout = { 
-        title: 'Stadium',
+        title: 'Stadium Capcity',
       };
       
       var config = {responsive: true}
@@ -51,4 +70,38 @@ d3.json("/api/stadiums").then((data) => {
   }
   
   buildStadiumChart("Stadium")
+
+  function buildRecordChart(record) {
+  
+    console.log(record);
+  
+    d3.json(`/api/record/${record}`).then((data) => {
+  
+      var trace1 = {
+        type: 'bar',
+        x: data['Team'],
+        y: data['Percentage'],
+        marker: {
+            color: '#C8A2C8',
+        }
+      };
+      
+      var data = [ trace1 ];
+      
+      var layout = { 
+        title: 'Win Percentage by Team',
+      };
+      
+      var config = {responsive: true}
+      
+      Plotly.newPlot('bar', data, layout, config );
+    
+    })
+  }
+  
+  function optionRecordChanged(newRecord) {
+    buildRecordChart(newRecord);
+  }
+  
+  buildRecordChart("Record")
   
