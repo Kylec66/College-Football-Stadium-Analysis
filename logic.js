f7e2e93b9c880c928959bc770ec1175e6149905a
Kyle Carlson <83102597+Kylec66@users.noreply.github.com>
2021-10-07 15:42:42 -0700

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,107 @@
+d3.json("/api/Stadiums").then((data) => {
+
+    console.log(data)
+  
+    // $(document).ready(function() {
+    $('#example').DataTable({
+        data: data,
+        columns: [
+            { title: "Stadium" },
+            { title: "Latitude" },
+            { title: "Longitude" },
+            { title: "City" },
+            { title: "State" },
+            { title: "Team" },
+            { title: "Conference" },
+            { title: "Capacity" },
+        ]
+    });
+  })
+  
+d3.json("/api/record").then((data) => {
+
+    console.log(data)
+  
+    // $(document).ready(function() {
+    $('#example').DataTable({
+        data: data,
+        columns: [
+            { title: "Team" },
+            { title: "Won" },
+            { title: "Lost" },
+            { title: "Tied" },
+            { title: "Percentage" },
+            { title: "Years" },
+            { title: "Total_Games" },
+            { title: "Conference" },
+        ]
+    });
+  })
+  function buildStadiumChart(Conference) {
+  
+    console.log(Conference);
+  
+    d3.json(`/api/stadiums/${Conference}`).then((data) => {
+  
+      var trace1 = {
+        type: 'bar',
+        x: data['Team'],
+        y: data['Capacity'],
+        marker: {
+            color: '#42e6f5',
+        }
+      };
+      
+      var data = [ trace1 ];
+      
+      var layout = { 
+        title: 'Stadium Capacity',
+      };
+      
+      var config = {responsive: true}
+      
+      Plotly.newPlot('bar', data, layout, config );
+    
+    })
+  }
+  
+  function optionStadiumChanged(newStadium) {
+    buildStadiumChart(newStadium);
+  }
+  
+  buildStadiumChart("Stadium")
+
+  function buildRecordChart(team) {
+  
+    console.log(team);
+  
+    d3.json(`/api/record/${team}`).then((data) => {
+  
+      var trace1 = {
+        type: 'bar',
+        x: data['Team'],
+        y: data['Percentage'],
+        marker: {
+            color: '#42e6f5',
+        }
+      };
+      
+      var data = [ trace1 ];
+      
+      var layout = { 
+        title: 'Win Percentage by Team',
+      };
+      
+      var config = {responsive: true}
+      
+      Plotly.newPlot('v-bar', data, layout, config );
+    
+    })
+  }
+  
+  function optionRecordChanged(newRecord) {
+    buildRecordChart(newRecord);
+  }
+  
+  buildRecordChart("Record")
+  
